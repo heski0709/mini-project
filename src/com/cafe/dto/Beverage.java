dfd282ae7a03a0997f2b76181e3c761e20b4cc8b
heeseung <heski0709@gmail.com>
2023-04-21 19:51:30 +0900

    [김희승] Beverage 클래스 생성

--- src/com/cafe/dto/Beverage.java
+++ src/com/cafe/dto/Beverage.java
@@ -1,5 +1,15 @@
 package com.cafe.dto;
 
 public class Beverage extends OptionDTO {
-    
+
+    public Beverage(String name, int price, String size, 
+            int addShot, boolean isSyrup, boolean isIce, 
+            boolean isCold, boolean isSizeUp) {
+        super(name, price, size, addShot, isSyrup, isIce, isCold, isSizeUp);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
