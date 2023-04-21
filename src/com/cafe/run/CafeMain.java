ad79fd25a565930a056d06b66233525472ff066b
JISU <dkljjh10@gmail.com>
2023-04-21 19:52:29 +0900

    "최신"

--- src/com/cafe/run/CafeMain.java
+++ src/com/cafe/run/CafeMain.java
@@ -3,8 +3,6 @@ package com.cafe.run;
 public class CafeMain {
     public static void main(String[] args) {
         
-        new CafeMain();
-//        new CafeMain2();
-        System.out.println("끝");
+
     }
 }
