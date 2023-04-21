c0d2f6a9f85e518e687d3f3b6a17bda9ad52bbc9
heeseung <heski0709@gmail.com>
2023-04-21 19:51:56 +0900

    [김희승] TypeDTO 클래스 생성

--- src/com/cafe/dto/TypeDTO.java
+++ src/com/cafe/dto/TypeDTO.java
@@ -1,5 +1,26 @@
 package com.cafe.dto;
 
 public class TypeDTO {
+    private String name;
+    private int price;
 
+    protected TypeDTO() {}
+
+    protected TypeDTO(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name + ", price=" + price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
