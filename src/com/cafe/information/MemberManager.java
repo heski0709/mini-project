ba0e3c8e3be66de0980df012605651c9c20b8a34
jaehyeong_ <pshong2012@gmail.com>
2023-04-24 17:26:26 +0900

    HONG JAE HYEONG 회원정보등록 및 멤버등급설정

---
+++ src/com/cafe/information/MemberManager.java
@@ -0,0 +1,18 @@
+package com.cafe.information;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberManager {
+
+	List<MemberDTO> memberlist = new ArrayList<>();
+	public void init() {
+		memberlist.add(new WelcomeMember(01011111111, 600, "Welcome")); 
+		memberlist.add(new GreenMember(01022222222, 1200, "Green")); 
+		memberlist.add(new GoldMember(01033333333, 1800, "Gold")); 
+		
+	}
+	
+	
+			
+}
